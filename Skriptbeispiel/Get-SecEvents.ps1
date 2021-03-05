945104ae97ec9a2f6816dbd755ae5566d4a9148b
Stefan Ober <StefanO@ppedv.de>
2021-03-05 11:31:16 +0100

    Verbose Ausgabe hinzugefügt

--- Skriptbeispiel/Get-SecEvents.ps1
+++ Skriptbeispiel/Get-SecEvents.ps1
@@ -8,4 +8,7 @@ param(
 [int]$Newest = 5
 )
 
+Write-Verbose -Message "EventID: $EventId"
+Write-Verbose -Message "Computername: $Computername"
+Write-Verbose -Message "Newest: $Newest"
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventID -eq $EventId | Select-Object -First $Newest
