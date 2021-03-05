fa2087050d06df128207758a378e6652ab5340fa
Stefan Ober <StefanO@ppedv.de>
2021-03-05 11:21:27 +0100

    Einfache Skript Version mit optionalen und Pflicht Parametern

---
+++ Skriptbeispiel/Get-SecEvents.ps1
@@ -0,0 +1,11 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[string]$Computername = "localhost",
+
+[int]$Newest = 5
+)
+
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventID -eq $EventId | Select-Object -First $Newest
