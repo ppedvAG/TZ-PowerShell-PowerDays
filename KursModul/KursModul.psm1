2528f1985cb3b8bfa55142092d97c5738b45ffe3
Stefan Ober <StefanO@ppedv.de>
2021-03-08 12:22:55 +0100

    Konvertierung eines Skriptes in eine Function um es dann als Powershell Modul verwenden zu können

---
+++ KursModul/KursModul.psm1
@@ -0,0 +1,53 @@
+﻿
+
+function Get-SecEvents
+{
+<#
+.Synopsis
+   Skript zum Abfragen von von Anmeldebezogenene Events
+.DESCRIPTION
+   Dieses Skript ermöglicht uns Events vom Security Log abzufragen.
+.PARAMETER EventId
+   Es werden folgende Werte unterstützt
+   4624 : Anmeldung
+   4625 : fehlgeschlagene Anmeldung
+   4634 : Abmeldung
+.EXAMPLE
+   Skriptbeispiel\Get-SecEvents.ps1 -EventId 4624
+
+   Index Time          EntryType   Source                 InstanceID Message
+   ----- ----          ---------   ------                 ---------- -------
+   50089 Mrz 08 12:04  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet...
+   50086 Mrz 08 12:04  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet...
+   50082 Mrz 08 12:03  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet...
+   50078 Mrz 08 12:02  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet...
+   50075 Mrz 08 12:02  SuccessA... Microsoft-Windows...         4624 Ein Konto wurde erfolgreich angemeldet...
+
+   Bei dieser Verwendung werden die akutellsten fünf Anmeldeevents abgefragt
+#>
+[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[ValidateSet(4624,4625,4634)] #Lässt nur die angegebenen Werte zu
+[int]$EventId,
+
+[ValidateScript({Test-NetConnection -Computername $PSItem -CommonTcpPort WinRM -InformationLevel Quiet})]
+[string]$Computername = "localhost",
+
+
+[ValidateRange(5,10)]  
+[int]$Newest = 5
+)
+
+Write-Verbose -Message "EventID: $EventId"
+Write-Verbose -Message "Computername: $Computername"
+Write-Verbose -Message "Newest: $Newest"
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventID -eq $EventId | Select-Object -First $Newest
+}
+
+function Test-Export
+{
+    #Wird im Get-Command nicht angzeigt da diese Function im Export-ModuleMember nicht angegeben ist
+}
+
+Export-ModuleMember -Function "Get-SecEvents"
