c11d6511237314af4979f23358c4da390a5036e6
Stefan Ober <StefanO@ppedv.de>
2021-03-10 10:30:36 +0100

    Write-Debug Example hinzugefügt

---
+++ DebuggingExample/DebuggingExample.ps1
@@ -0,0 +1,17 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$text
+)
+
+#Write Verbose gibt zusatz Informationen die im Normalfall unterdrückt werden. Skript aufruf mit -Verbose !
+Write-Verbose -Message "Folgender WErt wurde übergeben: $text"
+
+Write-Debug -Message "Vor erster Ausgabe"
+Write-Host -Object $text -ForegroundColor DarkMagenta
+
+Write-Debug -Message "Vor zweiter Ausgabe"
+Write-Host -Object $text -ForegroundColor Magenta
+
+Write-Debug -Message "Vor dritter Ausgabe"
+Write-Host -Object $text -ForegroundColor Green
